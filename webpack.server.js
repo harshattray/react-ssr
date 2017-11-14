323706d33aec7ea2ec10fd61fd2474f3455ace28
Harsha Attray <harsh.attray@gmail.com>
2017-11-14 13:09:47 +0530

    reduced server bundle file size

--- webpack.server.js
+++ webpack.server.js
@@ -3,12 +3,13 @@
 * @Date:   2017-11-13T16:44:23+05:30
 * @Filename: webpack.server.js
  * @Last modified by:   harsha
- * @Last modified time: 2017-11-14T12:47:42+05:30
+ * @Last modified time: 2017-11-14T13:06:42+05:30
 * @License: Apache License v2.0
 */
 const path = require('path');
 const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base')
+const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 
 const config = {
@@ -21,8 +22,9 @@ const config = {
   output:{
     filename: 'bundle.js',
     path: path.resolve(__dirname,'build')
-  }
+  },
   //tells webpack to run babel on every file it runs through
+  externals:[webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig,config);
